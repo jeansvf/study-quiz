d4655cdba34f8420582d9c484fb66ee135d5fd4d
jeansvf <jeansvf@gmail.com>
2023-04-07 18:20:11 -0300

    added hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Account from "./pages/Account";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
@@ -8,15 +8,15 @@ import Register from "./pages/Register";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/my-quizzes" element={<MyQuizzes />} />
-        <Route path="/quiz/:quizId" element={<Quiz />} />
-      </Routes>
-    </BrowserRouter>
+      <HashRouter>
+        <Routes>
+            <Route path="/account" element={<Account />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/my-quizzes" element={<MyQuizzes />} />
+            <Route path="/quiz/:quizId" element={<Quiz />} />
+            <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </HashRouter>
   )
 }
